6f986ae1fb572e3510d48eb37d774df56eb70c40
jeffkuo8321 <jeffkuo8321@gmail.com>
2019-09-01 08:49:38 +0800

    modified default  value for auto save img parameter.

--- IdiomExam/Properties/Settings.Designer.cs
+++ IdiomExam/Properties/Settings.Designer.cs
@@ -85,7 +85,7 @@ namespace IdiomTrainer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public string autoSaveImg {
             get {
                 return ((string)(this["autoSaveImg"]));
